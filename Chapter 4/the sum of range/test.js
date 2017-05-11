4b9e999b0f6413d1de04b807aa5b96b44c78b804
Enes K <enesmil@gmail.com>
2017-05-11 15:28:21 +0200

    First part of sum of range complete.

---
+++ Chapter 4/the sum of range/test.js
@@ -0,0 +1,17 @@
+let start = 1;
+let end = 10;
+
+let arr = [];
+
+function range (start,end) {
+
+for (var i = start; i <= end; i++) {
+
+	arr.push(i);
+}
+
+console.log(arr);
+}
+range(start, end);
+
+/* Part 1 complete */
